f09f7649cb462722d82b5ac1521137f3556cafde
DevWithMaj <0majesity@gmail.com>
2023-11-07 12:59:58 -0500

    Initial Commit

---
+++ Stardew CHAOS Mod/BuffHandler.cs
@@ -0,0 +1,54 @@
+﻿using StardewValley;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stardew_CHAOS_Mod
+{
+    internal class BuffHandler
+    {
+
+        public enum Buffs : int
+        {
+            DARKNESS=26,
+            FROZEN=19,
+            SLIME=13,
+            TIPSY=17,
+            BURNED=12,
+            SPEED=9
+        }
+
+        public static void GiveBuff(Buffs buffId, int duration)
+        {
+            Game1.playSound("debuffHit");
+            int id = (int)buffId;
+            Buff b = new Buff(id);
+            b.millisecondsDuration = duration;
+            Game1.buffsDisplay.addOtherBuff(b);
+        }
+
+        public static void GiveBuff(Buffs buffId, int duration, int strength)
+        {
+            Game1.playSound("debuffHit");
+            int id = (int)buffId;
+            Buff b = new Buff(id);
+            b.millisecondsDuration = duration;
+            if (buffId == Buffs.SPEED)
+            {
+                b.buffAttributes[9] = strength;
+            }
+            Game1.buffsDisplay.addOtherBuff(b);
+        }
+
+        public static void GiveBuff(int buffId, int duration)
+        {
+            Game1.playSound("debuffHit");
+            Buff b = new Buff(buffId);
+            b.millisecondsDuration = duration;
+            Game1.buffsDisplay.addOtherBuff(b);
+        }
+
+    }
+}
